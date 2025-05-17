8d0a2a48bc39e80f70bddb68eac5ea166c1f550d
ericflores108 <eflorty108@gmail.com>
2025-05-17 07:50:29 -0700

    fix go imports

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,21 @@ end
 vim.opt.rtp:prepend(lazypath)
 vim.opt.conceallevel = 1
 vim.opt.relativenumber = true
-
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+    for _, res in pairs(result or {}) do
+      for _, action in pairs(res.result or {}) do
+        if action.edit then
+          vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+        end
+      end
+    end
+  end,
+})
 local lazy_config = require "configs.lazy"
 -- load plugins
 require("lazy").setup({
